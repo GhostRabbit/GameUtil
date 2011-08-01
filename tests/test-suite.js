40eb8354bf6a319370ce1f712992143706fa4cc3
ghostrabbit <mattias@fredsberg.se>
2011-08-01 08:08:14 +0000

    Improved reporting for test-suite

--- tests/test-suite.js
+++ tests/test-suite.js
@@ -1,12 +1,14 @@
+var startTime = new Date();
 var fileNames = ['./CustomDieTest.js', './PoolTest.js', './RandomScramblerTest.js', './ZombieDieTest.js', './ZombieDicePoolTest.js', './TurnTest.js'];
 var testCount = 0;
 for (var i = 0; i < fileNames.length; i++) {
     var testFile = require(fileNames[i]);
-    console.log(fileNames[i] + ':');
+    console.log(require.resolve(fileNames[i]) + ':');
     for (var key in testFile) {
         console.log('    ' + key);
         testFile[key]();
         testCount++;
     }
 }
-console.log("All done, executed", testCount, "tests in", fileNames.length, "files");
+var duration = new Date() - startTime;
+console.log("Success, executed", testCount, "tests in", fileNames.length, "files. Took", duration, "ms.");
